56e4d20455e9743302724e5fad335fa1d0b91638
Renato Abreu <renatoabreu1196@gmail.com>
2018-01-25 20:21:32 +0000

    improvements to dimensions

--- lidaco/readers/Galion.py
+++ lidaco/readers/Galion.py
@@ -1,10 +1,19 @@
 from ..core.Reader import Reader
-import pprint
+import numpy as np
+import datetime
 
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i + n]
+
+
+def process_time(timestamp):
+    y, m, d = timestamp[:10].split('-')
+    h, min_, s = timestamp[11:19].split(':')
+    ms = timestamp[20:]
+    dt = datetime.datetime(int(y), int(m), int(d), int(h), int(min_), int(s), int(ms) * 1000)
+    return dt.timestamp()
 
 
 class Galion(Reader):
@@ -18,9 +27,14 @@ class Galion(Reader):
     def output_filename(self, filename):
         return filename[:-4]
 
+    def required_params(self):
+        return ['n_gates', 'range_gates', 'measurement_scenarios']
+
     def read_to(self, output_dataset, input_filepath, configs, index):
         with open(input_filepath) as file:
-            range_gates = 42
+            nr_gates = configs['parameters']['n_gates']
+            range_gates = configs['parameters']['range_gates']
+            measurement_scenarios = configs['parameters']['measurement_scenarios']
             raw_file = file.readlines()
 
             # if we can assume that the metadata is always the same length
@@ -29,25 +43,30 @@ class Galion(Reader):
             data = raw_file[6:]
 
             # It is useful to have defined the number of ranges per scan
-            scans = list(chunks([row.strip().split('\t') for row in data], range_gates))
-            pprint.pprint(scans)
+            scans = np.array(list(chunks([row.strip().split('\t') for row in data], 42)))
 
             # create the dimensions
-            output_dataset.createDimension('range', range_gates)
-            output_dataset.createDimension('time', None)
+            output_dataset.createDimension('range', nr_gates)
+            output_dataset.createDimension('time', len(scans))
 
             # create the coordinate variables
             # range
             range1 = output_dataset.createVariable('range', 'f4', ('range',))
             range1.units = 'm'
             range1.long_name = 'range_gate_distance_from_lidar'
-            range1[:] = 0
+            range1[:] = np.array(range_gates.split(';')).astype(float)
             range1.comment = ''
 
             # time
             time = output_dataset.createVariable('time', 'f4', ('time',))
             time.units = 's'
-            time.long_name = 'seconds since 1904-01-01 12:00AM UTC'
+            start_time_kv = metadata[4]
+            start_time_str = start_time_kv[start_time_kv.find('\t') + 1:start_time_kv.find('\n')]
+            start_time_seconds = process_time(start_time_str)
+            time.long_name = 'seconds since ' + start_time_str
+            # array manipulation to obtain timestamps
+            timestamps = (scans[:, :, 3])[:, 0]
+            time[:] = np.array(list(map(lambda x: process_time(x) - start_time_seconds, timestamps)))
             time.comment = ''
 
             # create the beam steering variables
@@ -71,7 +90,8 @@ class Galion(Reader):
             yaw.units = 'degrees'
             yaw.long_name = 'lidar_yaw_angle'
             yaw[:] = 0
-            yaw.comment = 'The home position of the lidar has been configured in a way that 0 azimuth corresponds to north.'
+            yaw.comment = 'The home position of the lidar has been configured in a way that 0 azimuth corresponds to ' \
+                          'north. '
             yaw.accuracy = ''
 
             pitch = output_dataset.createVariable('pitch', 'f4')
@@ -100,12 +120,3 @@ class Galion(Reader):
             scan_id.units = 'none'
             scan_id.long_name = 'scan_id_of_the_measurement'
             scan_id[:] = 1
-
-            accumulation_time = output_dataset.createVariable('accumulation_time', 'f4')
-            accumulation_time.units = 'seconds'
-            accumulation_time.long_name = 'time_for_spectral_accumulation'
-            accumulation_time[:] = 2
-            accumulation_time.comment = ''
-
-            # create the measurement variables VEL, CNR, WIDTH
-
