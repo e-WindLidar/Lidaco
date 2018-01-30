fce09691b1b47ac1cee8adf2d2323c448a8a2b48
Renato Abreu <renatoabreu1196@gmail.com>
2018-01-30 23:21:44 +0000

    second approach almost done

--- lidaco/readers/Galion.py
+++ lidaco/readers/Galion.py
@@ -16,6 +16,64 @@ def process_time(timestamp):
     return dt.timestamp()
 
 
+def create_variables(group, azimuth, elevation, _yaw, _pitch, _roll, doppler, intensity):
+    # create the beam steering variables
+    # azimuth and elevation
+    azimuth_angle = group.createVariable('azimuth_angle1', 'f4', 'time')
+    azimuth_angle.units = 'degrees'
+    azimuth_angle.long_name = 'azimuth_angle_of_lidar beam'
+    azimuth_angle[:] = azimuth
+    azimuth_angle.comment = ''
+    azimuth_angle.accuracy = ''
+    azimuth_angle.accuracy_info = ''
+
+    elevation_angle = group.createVariable('elevation_angle', 'f4', 'time')
+    elevation_angle.units = 'degrees'
+    elevation_angle.long_name = 'elevation_angle_of_lidar beam'
+    elevation_angle[:] = elevation
+    elevation_angle.comment = ''
+    elevation_angle.accuracy = ''
+    elevation_angle.accuracy_info = ''
+
+    # yaw, pitch, roll
+    yaw = group.createVariable('yaw', 'f4', 'time')
+    yaw.units = 'degrees'
+    yaw.long_name = 'lidar_yaw_angle'
+    yaw[:] = _yaw
+    yaw.comment = 'The home position of the lidar has been configured in a way that 0 azimuth corresponds to ' \
+                  'north. '
+    yaw.accuracy = ''
+
+    pitch = group.createVariable('pitch', 'f4', 'time')
+    pitch.units = 'degrees'
+    pitch.long_name = 'lidar_pitch_angle'
+    pitch[:] = _pitch
+    pitch.comment = ''
+    pitch.accuracy = ''
+    pitch.accuracy_info = 'No information on pitch accuracy available.'
+
+    roll = group.createVariable('roll', 'f4', 'time')
+    roll.units = 'degrees'
+    roll.long_name = 'lidar_roll_angle'
+    roll[:] = _roll
+    roll.comment = ''
+    roll.accuracy = ''
+    roll.accuracy_info = 'No information on roll accuracy available.'
+
+    # measurement variables
+    # TODO verify metadata
+    # Doppler and Intensity
+    DOPPLER = group.createVariable('DOPPLER', 'f4', ('time', 'range'))
+    DOPPLER.units = 'm.s-1'
+    DOPPLER.long_name = 'doppler'
+    DOPPLER[:] = doppler
+
+    INTENSITY = group.createVariable('INTENSITY', 'f4', ('time', 'range'))
+    INTENSITY.units = 'm.s-1'
+    INTENSITY.long_name = 'intensity'
+    INTENSITY[:] = intensity
+
+
 class Galion(Reader):
 
     def __init__(self):
@@ -72,49 +130,6 @@ class Galion(Reader):
             time[:] = np.array(list(map(lambda x: process_time(x) - start_time_seconds, timestamps)))
             time.comment = ''
 
-            # create the beam steering variables
-            # azimuth and elevation
-            azimuth_angle = output_dataset.createVariable('azimuth_angle', 'f4', 'time')
-            azimuth_angle.units = 'degrees'
-            azimuth_angle.long_name = 'azimuth_angle_of_lidar beam'
-            azimuth_angle[:] = (scans[:, :, 4])[:, 0]
-            azimuth_angle.comment = ''
-            azimuth_angle.accuracy = ''
-            azimuth_angle.accuracy_info = ''
-
-            elevation_angle = output_dataset.createVariable('elevation_angle', 'f4', 'time')
-            elevation_angle.units = 'degrees'
-            elevation_angle.long_name = 'elevation_angle_of_lidar beam'
-            elevation_angle[:] = (scans[:, :, 5])[:, 0]
-            elevation_angle.comment = ''
-            elevation_angle.accuracy = ''
-            elevation_angle.accuracy_info = ''
-
-            # yaw, pitch, roll
-            yaw = output_dataset.createVariable('yaw', 'f4', 'time')
-            yaw.units = 'degrees'
-            yaw.long_name = 'lidar_yaw_angle'
-            yaw[:] = np.zeros(len(scans))
-            yaw.comment = 'The home position of the lidar has been configured in a way that 0 azimuth corresponds to ' \
-                          'north. '
-            yaw.accuracy = ''
-
-            pitch = output_dataset.createVariable('pitch', 'f4', 'time')
-            pitch.units = 'degrees'
-            pitch.long_name = 'lidar_pitch_angle'
-            pitch[:] = (scans[:, :, 6])[:, 0]
-            pitch.comment = ''
-            pitch.accuracy = ''
-            pitch.accuracy_info = 'No information on pitch accuracy available.'
-
-            roll = output_dataset.createVariable('roll', 'f4', 'time')
-            roll.units = 'degrees'
-            roll.long_name = 'lidar_roll_angle'
-            roll[:] = (scans[:, :, 7])[:, 0]
-            roll.comment = ''
-            roll.accuracy = ''
-            roll.accuracy_info = 'No information on roll accuracy available.'
-
             # create the data variables
             scan_type = output_dataset.createVariable('scan_type', 'i', 'time')
             scan_type.units = 'none'
@@ -124,34 +139,35 @@ class Galion(Reader):
             scan_id.units = 'none'
             scan_id.long_name = 'scan_id_of_the_measurement'
 
-            # measurement variables
-            # TODO verify metadata
-            # Doppler and Intensity
-            DOPPLER = output_dataset.createVariable('DOPPLER', 'f4', ('time', 'range'))
-            DOPPLER.units = 'm.s-1'
-            DOPPLER.long_name = 'doppler'
-            DOPPLER[:] = (scans[:, :, 1])[:]
-
-            INTENSITY = output_dataset.createVariable('INTENSITY', 'f4', ('time', 'range'))
-            INTENSITY.units = 'm.s-1'
-            INTENSITY.long_name = 'intensity'
-            INTENSITY[:] = (scans[:, :, 2])[:]
+            create_variables(output_dataset, (scans[:, :, 4])[:, 0], (scans[:, :, 5])[:, 0], np.zeros(len(scans)),
+                             (scans[:, :, 6])[:, 0], (scans[:, :, 7])[:, 0],
+                             (scans[:, :, 1]), (scans[:, :, 2]))
 
             invalid_scans = 0
             scan_index = 1
             for s in measurement_scenarios:
                 long_name = s['scenario']
                 _type = int(s['type'])
-                scans = s['scans']
+                records = s['scans']
                 if long_name == 'INVALID':
-                    split_scans = scans.split(';')
+                    split_scans = records.split(';')
                     for ss in split_scans:
                         invalid_scans += int(ss.split('-')[1]) - int(ss.split('-')[0]) + 1
                 else:
-                    split_scans = scans.split(';')
+                    scan_group = output_dataset.createGroup('scan_' + str(scan_index) + '_' + long_name)
+                    _azimuth = np.zeros(len(scans))
+                    _elevation = np.zeros(len(scans))
+                    _yaw = np.zeros(len(scans))
+                    _pitch = np.zeros(len(scans))
+                    _roll = np.zeros(len(scans))
+                    _doppler = np.zeros(shape=[len(scans), int(nr_gates)])
+                    _intensity = np.zeros(shape=[len(scans), int(nr_gates)])
+                    split_scans = records.split(';')
                     for ss in split_scans:
                         initial_index = int(ss.split('-')[0]) - (invalid_scans + 1)
                         final_index = int(ss.split('-')[1]) - (invalid_scans + 1)
                         scan_type[initial_index:final_index + 1] = _type
                         scan_id[initial_index:final_index + 1] = scan_index
+                        _azimuth[initial_index:final_index + 1] = scans[initial_index:final_index+1, :, 4][:, 0]
+                    create_variables(scan_group, _azimuth, _elevation, _yaw, _pitch, _roll, _doppler, _intensity)
                     scan_index += 1
