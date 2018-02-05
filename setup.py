b63c1f2a20786033e0800364eee0ac2d328baf28
Daniel Fernandes Gomes <danfergo@gmail.com>
2018-02-05 16:31:27 +0000

    Saving pip/python package required files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lidaco',
+    packages=find_packages(),
+    version='0.0.11',
+    description='Wind Lidar Data Converter.',
+    author='e-WindLidar',
+    scripts=['bin/lidaco'],
+    author_email='e-windlidar@googlegroups.com',
+    url='https://github.com/e-WindLidar/Lidaco',  # use the URL to the github repo
+    # download_url='https://github.com/e-WindLidar/Lidaco/archive/v0.0.1.tar.gz',
+    keywords=['wind', 'lidar', 'data', 'converter'],  # arbitrary keywords
+    classifiers=[],
+    install_requires=[
+        'pyyaml',
+        'netCDF4',
+        'lxml'
+    ]
+)
