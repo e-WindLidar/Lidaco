f024b2e9c6d38cfae775cc76391f7738b5893433
Tobias Klaas <tobias.klaas@iee.fraunhofer.de>
2018-06-11 11:47:02 +0200

    Updated version without fixed variables in the code
    This version does not use fixed varibles for location or pitch, yaw, roll in the code anymore. It fully relies on those variables to be defined in the yaml config files.

--- lidaco/readers/Windscanner.py
+++ lidaco/readers/Windscanner.py
@@ -1,46 +1,26 @@
 from ..core.Reader import Reader
+from datetime import datetime, timedelta
+import numpy as np
 
 
 class Windscanner(Reader):
 
     def __init__(self):
-        super().__init__(True)
+        super().__init__(False)
 
     def accepts_file(self, filename):
-        return filename.endswith('.txt') & (len(filename) > 14)
-
-    def group_id(self, filename):
-        return filename[:14]
+        return filename.endswith('wind.txt') & (len(filename) > 14)
 
     def output_filename(self, timestamp):
-        return timestamp
+        return timestamp[:-9]
 
-    def required_params(self):
-        return {
-            'position': ['x', 'y', 'z']
-        }
-
-    def read_to(self, output_dataset, input_filepaths, configs, appending):
-        scanner_file, system_file, wind_file = input_filepaths
-
-        x = configs['parameters']['position']['x']
-        y = configs['parameters']['position']['y']
-        z = configs['parameters']['position']['z']
-
-
+    def read_to(self, output_dataset, input_filepaths, parameters, appending):
+        wind_file = input_filepaths
 
         with open(wind_file) as f:
             wind_file_data = f.readlines()
 
-        with open(system_file) as f:
-            system_file_data = f.readlines()
-
-        with open(scanner_file) as f:
-            scanner_file_data = f.readlines()
-
         wind_file_data = list(zip(*[row.strip().split(';') for row in wind_file_data]))
-        system_file_data = list(zip(*[row.strip().split(';') for row in system_file_data]))
-        scanner_file_data = list(zip(*[row.strip().split(';') for row in scanner_file_data]))
 
         if not appending:
 
@@ -61,118 +41,16 @@ class Windscanner(Reader):
             range1.comment = ''
 
             # time
-            time = output_dataset.createVariable('time', 'f4', ('time',))
+            time = output_dataset.createVariable('time', str, ('time',))
             time.units = 's'
-            time.long_name = 'seconds since 1904-01-01 12:00AM UTC'
+            time.long_name = 'Time UTC in ISO 8601 format yyyy-mm-ddThh:mm:ssZ'
             time.comment = ''
-
-            # create the beam steering variables
-
-            # azimuth and elevation
-            azimuth_angle = output_dataset.createVariable('azimuth_angle', 'f4', ('time'))
-            azimuth_angle.units = 'degrees'
-            azimuth_angle.long_name = 'azimuth_angle_of_lidar beam'
-            azimuth_angle.comment = ''
-            azimuth_angle.accuracy = ''
-            azimuth_angle.accuracy_info = ''
-
-            #  only applicable for PPI or sweeping CT
-            azimuth_sweep = output_dataset.createVariable('azimuth_sweep', 'f4')
-            azimuth_sweep.units = 'degrees'
-            azimuth_sweep.long_name = 'azimuth_sector_swept_during_accumulation'
-            azimuth_sweep.comment = ''
-            azimuth_sweep[:] = 0
-            azimuth_sweep.accuracy = ''
-            azimuth_sweep.accuracy_info = ''
-
-            elevation_angle = output_dataset.createVariable('elevation_angle', 'f4', ('time'))
-            elevation_angle.units = 'degrees'
-            elevation_angle.long_name = 'elevation_angle_of_lidar beam'
-            elevation_angle.comment = ''
-            elevation_angle.accuracy = ''
-            elevation_angle.accuracy_info = ''
-
-            #  only applicable for RHI or sweeping CT
-            elevation_sweep = output_dataset.createVariable('elevation_sweep', 'f4')
-            elevation_sweep.units = 'degrees'
-            elevation_sweep.long_name = 'elevation_sector_swept_during_accumulation'
-            elevation_sweep.comment = 'Elevation sweeping from approximately 0 to 15 degrees.'
-            elevation_sweep[:] = 2
-            elevation_sweep.accuracy = ''
-            elevation_sweep.accuracy_info = ''
-
-            # yaw, pitch, roll
-            yaw = output_dataset.createVariable('yaw', 'f4')
-            yaw.units = 'degrees'
-            yaw.long_name = 'lidar_yaw_angle'
-            yaw[:] = 0
-            yaw.comment = 'The home position of the lidar has been configured in a way that 0 azimuth corresponds to north.'
-            yaw.accuracy = ''
-            yaw.accuracy_info = 'No information on yaw accuracy available.'
-
-            pitch = output_dataset.createVariable('pitch', 'f4')
-            pitch.units = 'degrees'
-            pitch.long_name = 'lidar_pitch_angle'
-            pitch[:] = 0
-            pitch.comment = ''
-            pitch.accuracy = ''
-            pitch.accuracy_info = 'No information on pitch accuracy available.'
-
-            roll = output_dataset.createVariable('roll', 'f4')
-            roll.units = 'degrees'
-            roll.long_name = 'lidar_roll_angle'
-            roll[:] = 0
-            roll.comment = ''
-            roll.accuracy = ''
-            roll.accuracy_info = 'No information on roll accuracy available.'
-
-            # create the location variables x,y,z
-            position_x = output_dataset.createVariable('position_x', 'f4')
-            position_x.units = 'm'
-            position_x.long_name = 'x_position_of_lidar'
-            position_x.comment = 'Measured by GPS. Coordinates are UTM 32 N.'
-            position_x[:] = x
-            position_x.accuracy = '<1m'
-            position_x.accuracy_info = 'Coordinates taken by a hand-held Garmin GPS device.'
-
-            position_y = output_dataset.createVariable('position_y', 'f4')
-            position_y.units = 'm'
-            position_y.long_name = 'y_position_of_lidar'
-            position_y.comment = 'Measured by GPS. Coordinates are UTM 32 N.'
-            position_y[:] = y
-            position_y.accuracy = '<1m'
-            position_y.accuracy_info = 'Coordinates taken by a hand-held Garmin GPS device.'
-
-            position_z = output_dataset.createVariable('position_z', 'f4')
-            position_z.units = 'meters'
-            position_z.long_name = 'z_position_of_lidar'
-            position_z.comment = 'Taken from a digital elevation model for the measured x,y position.'
-            position_z[:] = z
-            position_z.accuracy = '<1m'
-            position_z.accuracy_info = 'The digital elevation model was prepared under the use of airborne lidar data and verified against a federal digital elevation model.'
 
             # create the data variables
             scan_type = output_dataset.createVariable('scan_type', 'i')
             scan_type.units = 'none'
             scan_type.long_name = 'scan_type_of_the_measurement'
-            scan_type[:] = 4
 
-            scan_id = output_dataset.createVariable('scan_id', 'i')
-            scan_id.units = 'none'
-            scan_id.long_name = 'scan_id_of_the_measurement'
-            scan_id[:] = 1
-
-            accumulation_time = output_dataset.createVariable('accumulation_time', 'f4')
-            accumulation_time.units = 'seconds'
-            accumulation_time.long_name = 'time_for_spectral_accumulation'
-            accumulation_time[:] = 2
-            accumulation_time.comment = ''
-
-            n_spectra = output_dataset.createVariable('n_spectra', 'f4')
-            n_spectra.units = 'none'
-            n_spectra.long_name = 'number_of_pulses'
-            n_spectra[:] = 0
-            n_spectra.comment = 'No information available on number of spectra.'
 
             # create the measurement variables VEL, CNR, WIDTH
             VEL = output_dataset.createVariable('VEL', 'f4', ('time', 'range'))
@@ -196,11 +74,86 @@ class Windscanner(Reader):
             WIDTH.accuracy = ''
             WIDTH.accuracy_info = ''
 
-            # fill values from dataset
-            output_dataset.variables['time'][:] = [float(value) for value in wind_file_data[index_columns]]
-            output_dataset.variables['azimuth_angle'][:] = [float(value) for value in wind_file_data[6]]
-            output_dataset.variables['elevation_angle'][:] = [float(value) for value in wind_file_data[7]]
 
+            #%% get timestamps in ISO 8601 format
+            start_date = datetime(1904,1,1)
+            timestamp_seconds = [int(float(value.strip())) for value in wind_file_data[index_columns]]
+            timestamp_iso8601 = [ (start_date+timedelta(seconds=value)).isoformat()+'Z' for value in timestamp_seconds]
+            output_dataset.variables['time'][:] = np.array(timestamp_iso8601)
+            
+            #%% calculate azimuth and elevation sweeps
+            azimuth_angle_temp = [float(value) for value in wind_file_data[6]]
+            elevation_angle_temp = [float(value) for value in wind_file_data[7]]
+            azimuth_sweep_temp = np.insert(np.abs(np.diff(azimuth_angle_temp)),0,np.nan)
+            elevation_sweep_temp = np.insert(np.abs(np.diff(elevation_angle_temp)),0,np.nan)
+
+            #%% check if a sweep is existing and writing data accordingly
+            changing_azimuth = False
+            changing_elevation = False
+            beam_sweeping = (parameters['attributes']['beam_sweeping'] == 'true')
+            
+            if np.nanmedian(azimuth_sweep_temp) > 0:
+                changing_azimuth = True
+                azimuth_angle = output_dataset.createVariable('azimuth_angle', 'f4', ('time'))
+            
+                azimuth_sweep = output_dataset.createVariable('azimuth_sweep', 'f4', ('time'))
+                azimuth_sweep.units = 'degrees'
+                azimuth_sweep.long_name = 'azimuth_sector_swept_during_accumulation'
+                azimuth_sweep.comment = ''
+                azimuth_sweep.accuracy = ''
+                azimuth_sweep.accuracy_info = ''
+            
+                output_dataset.variables['azimuth_angle'][:] = azimuth_angle_temp
+                output_dataset.variables['azimuth_sweep'][:] = azimuth_sweep_temp
+                
+            else:
+                azimuth_angle = output_dataset.createVariable('azimuth_angle', 'f4')
+                output_dataset.variables['azimuth_angle'][:] = np.nanmedian(azimuth_angle_temp)
+            
+            
+            if np.nanmedian(elevation_sweep_temp) > 0:
+                changing_elevation = True
+                elevation_angle = output_dataset.createVariable('elevation_angle', 'f4', ('time'))
+        
+                elevation_sweep = output_dataset.createVariable('elevation_sweep', 'f4', ('time'))
+                elevation_sweep.units = 'degrees'
+                elevation_sweep.long_name = 'elevation_sector_swept_during_accumulation'
+                elevation_sweep.comment = 'Elevation sweeping from approximately 0 to 15 degrees.'
+                elevation_sweep.accuracy = ''
+                elevation_sweep.accuracy_info = ''
+        
+                output_dataset.variables['elevation_angle'][:] = elevation_angle_temp
+                output_dataset.variables['elevation_sweep'][:] = elevation_sweep_temp
+            else:
+                elevation_angle = output_dataset.createVariable('elevation_angle', 'f4')
+                output_dataset.variables['elevation_angle'][:] = np.nanmedian(elevation_angle_temp)
+            
+            azimuth_angle.units = 'degrees'
+            azimuth_angle.long_name = 'azimuth_angle_of_lidar beam'
+            azimuth_angle.comment = ''
+            azimuth_angle.accuracy = ''
+            azimuth_angle.accuracy_info = ''
+            
+            elevation_angle.units = 'degrees'
+            elevation_angle.long_name = 'elevation_angle_of_lidar beam'
+            elevation_angle.comment = ''
+            elevation_angle.accuracy = ''
+            elevation_angle.accuracy_info = ''
+
+            #%% setting scan_type according to sweeps            
+            if (not changing_azimuth) & (not changing_elevation): #case LOS
+                scan_type[:] = 1
+            elif (changing_azimuth) & (not changing_elevation) & (not beam_sweeping): #case DBS
+                scan_type[:] = 2
+            elif (changing_azimuth) & (not changing_elevation) & (beam_sweeping): #case PPI
+                scan_type[:] = 4
+            elif (not changing_azimuth) & (changing_elevation) & (beam_sweeping): #case RHI
+                scan_type[:] = 5
+            else: #case other
+                scan_type[:] = 0
+            
+         
+            #%% read vel, width, cnr out of dataset
             # e.g. radial velocity starts at 5th column and is then repeated every 9th column
             output_dataset.variables['VEL'][:, :] = list(
                 zip(*[[float(value) for value in row] for row in wind_file_data[index_columns + 5::4]]))
@@ -208,15 +161,29 @@ class Windscanner(Reader):
                 zip(*[[float(value) for value in row] for row in wind_file_data[index_columns + 6::4]]))
             output_dataset.variables['CNR'][:, :] = list(
                 zip(*[[float(value) for value in row] for row in wind_file_data[index_columns + 7::4]]))
-
-        else:
+            
+        #%% case appending
+        else: 
             index_columns = 4 - (len(wind_file_data) % 4)
-
             ntime = len(output_dataset.dimensions["time"])
+            
+            start_date = datetime(1904,1,1)
+            timestamp_seconds = [int(float(value.strip())) for value in wind_file_data[index_columns]]
+            timestamp_iso8601 = [ (start_date+timedelta(seconds=value)).isoformat()+'Z' for value in timestamp_seconds]
+            output_dataset.variables['time'][ntime:] = np.array(timestamp_iso8601)
+            
+            if 'azimuth_sweep' in output_dataset.variables :
+                azimuth_angle_temp = [float(value) for value in wind_file_data[6]]
+                azimuth_sweep_temp = np.insert(np.abs(np.diff(azimuth_angle_temp)),0,np.nan)
+                output_dataset.variables['azimuth_angle'][ntime:] = azimuth_angle_temp
+                output_dataset.variables['azimuth_sweep'][ntime:] = azimuth_sweep_temp
 
-            output_dataset.variables['time'][ntime:] = [float(value) for value in wind_file_data[index_columns]]
-            output_dataset.variables['azimuth_angle'][ntime:] = [float(value) for value in wind_file_data[6]]
-            output_dataset.variables['elevation_angle'][ntime:] = [float(value) for value in wind_file_data[7]]
+            if 'elevation_sweep' in output_dataset.variables :
+                elevation_angle_temp = [float(value) for value in wind_file_data[7]]
+                elevation_sweep_temp = np.insert(np.abs(np.diff(elevation_angle_temp)),0,np.nan)
+                output_dataset.variables['elevation_angle'][ntime:] = elevation_angle_temp
+                output_dataset.variables['elevation_sweep'][ntime:] = elevation_sweep_temp
+
 
             # e.g. radial velocity starts at 5th column and is then repeated every 9th column
             output_dataset.variables['VEL'][ntime:, :] = list(
