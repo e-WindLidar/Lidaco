decfc98ff960bc3becfa9eb58093d963c1f51732
Daniel Fernandes Gomes <danfergo@gmail.com>
2018-06-28 15:08:38 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name='lidaco',
     packages=find_packages(),
-    version='0.0.11',
+    version='0.0.15',
     description='Wind Lidar Data Converter.',
     author='e-WindLidar',
-    scripts=['bin/lidaco'],
+    entry_points={
+        'console_scripts': [
+            'lidaco = lidaco.__main__:main'
+        ],
+    },
+    # scripts=['bin/lidaco'],
     author_email='e-windlidar@googlegroups.com',
     url='https://github.com/e-WindLidar/Lidaco',  # use the URL to the github repo
     # download_url='https://github.com/e-WindLidar/Lidaco/archive/v0.0.1.tar.gz',
