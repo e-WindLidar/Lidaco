c2b9c509650df1caf1b45ab6be86fd4e90260c63
skulla1 <sebastian.kulla@iee.fraunhofer.de>
2018-07-23 15:07:21 +0200

    added Reader Windcubev1 & ZephIR300 ZephIR300 uses pandas for file processing ZephIR300 reads raw_data & 10min files All 3 reader save the Wiper Count

--- lidaco/readers/Windcubev1.py
+++ lidaco/readers/Windcubev1.py
@@ -1,8 +1,11 @@
+# Adapted from Windcubev2, but not yet tested
+
+import datetime
 import numpy as np
 from ..core.Reader import Reader
-import datetime
 
-class Windcubev2(Reader):
+
+class Windcubev1(Reader):
 
     def __init__(self):
         super().__init__(False)
@@ -32,70 +35,71 @@ class Windcubev2(Reader):
 
     def output_filename(self, filename):
         return filename[:-4]
-
+    
+    # remove this from params but set it manually here
 #    def required_params(self):
 #        return ['position_x_input', 'position_y_input', 'position_z_input']
 
     def read_to(self, output_dataset, input_filepath, configs, appending):
 
+
         # read file
         with open(input_filepath, encoding='latin-1') as f:
             data = f.readlines()
-
-            # get parameters from header
             filetype = input_filepath[-3:]
+            # get parameters from header
             temp_HeaderSize = int(data[0].split(sep='=')[1])
             parameters = [line[:-1].split(sep='=') for line in data[0:temp_HeaderSize]]
-            parameters = {line[0]: Windcubev2.str_to_num(line[1]) for line in parameters if len(line) == 2}
-            parameters['Altitudes (m)'] = [Windcubev2.str_to_num(element) for element in
-                                           parameters['Altitudes (m)'].strip().split('\t')]
-            
-            
-            
+            parameters = {line[0]: Windcubev1.str_to_num(line[1]) for line in parameters if len(line) == 2}
+            parameters['Altitudes(m)'] = [Windcubev1.str_to_num(element) for element in
+                                           parameters['Altitudes(m)'].strip().split('\t')]
+           
             if filetype == 'rtd':
                 parameters['first_timestamp'] = datetime.datetime.strptime(
-                    data[parameters['HeaderSize'] + 2].split('\t')[0][:-3],
-                    '%Y/%m/%d %H:%M:%S')
-            
+                    data[parameters['HeaderLength'] + 2].split('\t')[0][:-3],
+                    '%d/%m/%Y %H:%M:%S')
             else:
                 parameters['first_timestamp'] = datetime.datetime.strptime(
-                    data[parameters['HeaderSize'] + 2].split('\t')[0],
-                    '%Y/%m/%d %H:%M')
-            
+                    data[parameters['HeaderLength'] + 2].split('\t')[0],
+                    '%d/%m/%Y %H:%M:%S')
 
 
             # general data set description
-            output_dataset.site = parameters['Location']
+            output_dataset.site = parameters['Localisation']
 
             # create the dimensions
-            output_dataset.createDimension('range', len(parameters['Altitudes (m)']))
+            output_dataset.createDimension('range', len(parameters['Altitudes(m)']))
             output_dataset.createDimension('time', None)
 
             # create the coordinate variables
             range1 = output_dataset.createVariable('range', 'f4', ('range',))
             range1.units = 'm'
             range1.long_name = 'range_gate_distance_from_lidar'
-            range1[:] = np.array(parameters['Altitudes (m)'])
+            range1[:] = np.array(parameters['Altitudes(m)'])
+
+
 
             time = output_dataset.createVariable('time', str, ('time',))
             time.units = 's'
             time.long_name = 'seconds since ' + parameters['first_timestamp'].strftime('%Y/%m/%d') + ' 00:00:00'
+            
 
             # create the beam steering and location variables
             yaw = output_dataset.createVariable('yaw', 'f4')
             yaw.units = 'degrees'
             yaw.long_name = 'lidar_yaw_angle'
-            yaw[:] = parameters['DirectionOffset (°)']
+            yaw[:] = parameters['DirectionOffset(°)']
 
             pitch = output_dataset.createVariable('pitch', 'f4')
             pitch.units = 'degrees'
             pitch.long_name = 'lidar_pitch_angle'
-            pitch[:] = parameters['PitchAngle (°)']
+            pitch[:] = parameters['PitchAngle(°)']
 
             roll = output_dataset.createVariable('roll', 'f4')
             roll.units = 'degrees'
             roll.long_name = 'lidar_roll_angle'
-            roll[:] = parameters['RollAngle (°)']
+            roll[:] = parameters['RollAngle(°)']
+
 
             # create the data variables
             scan_type = output_dataset.createVariable('scan_type', 'i')
@@ -111,10 +115,11 @@ class Windcubev2(Reader):
             n_spectra = output_dataset.createVariable('n_spectra', 'f4')
             n_spectra.units = 'none'
             n_spectra.long_name = 'number_of_pulses'
-            n_spectra[:] = parameters['Pulses / Line of Sight']
+            n_spectra[:] = parameters['NumberOfAveragedShots']
 
-            # create the measurement variables
-            
+
+
+
             if filetype == 'rtd':
                 VEL = output_dataset.createVariable('VEL', 'f4', ('time', 'range'))
                 VEL.units = 'm.s-1'
@@ -132,7 +137,11 @@ class Windcubev2(Reader):
                 WS = output_dataset.createVariable('WS', 'f4', ('time', 'range'))
                 WS.units = 'm.s-1'
                 WS.long_name = 'mean of scalar wind speed'
-
+                
+                
+                
+                
+                
             CNR = output_dataset.createVariable('CNR', 'f4', ('time', 'range'))
             CNR.units = 'dB'
             CNR.long_name = 'carrier-to-noise ratio'
@@ -149,35 +158,34 @@ class Windcubev2(Reader):
             wiper.units = 'V'
             wiper.long_name = 'Wiper count Vbatt'
             
-            
+
             # fill values from dataset
-            data_timeseries = [row.strip().split('\t') for row in data[parameters['HeaderSize'] + 2:]]
+            data_timeseries = [row.strip().split('\t') for row in data[parameters['HeaderLength'] + 3:]]
             
+            # check if Windcubev2.util_process_time works here, should be the same as for Windcube v2
+            # check implementation of azimuth, elevation, ...
+            
+            #  check if this is correct
+            # e.g. radial velocity starts at 5th column and is then repeated every 9th column
             if filetype == 'rtd':
-                timestamp_input = [datetime.datetime.strptime(row[0][:-3],'%Y/%m/%d %H:%M:%S') for row in data_timeseries]
+                timestamp_input = [datetime.datetime.strptime(row[0][:-3],'%d/%m/%Y %H:%M:%S') for row in data_timeseries]
                 timestamp_iso8601 = [value.isoformat()+'Z' for value in timestamp_input]
                 output_dataset.variables['time'][:] = np.array(timestamp_iso8601)
                 output_dataset.variables['T_internal'][:] = [float(row[2]) for row in data_timeseries]
-                output_dataset.variables['wiper'][:] = [float(row[3]) for row in data_timeseries]
+                output_dataset.variables['wiper'][:] = [float(row[3]) for row in data_timeseries]       #needs to be validated with a rtd-File!!!!!!!!!!!!!!!!!!
+                output_dataset.variables['azimuth_angle'][:] = [float(row[1]) for row in data_timeseries]
+                output_dataset.variables['elevation_angle'][:] = [parameters['ScanAngle(°)'] for row in data_timeseries]
                 
-                output_dataset.variables['azimuth_angle'][:] = [float(row[1]) if row[1] != 'V' else 0 for row in
-                                                                data_timeseries]
-                output_dataset.variables['elevation_angle'][:] = [90 - parameters['ScanAngle (°)'] if row[1] != 'V' else 90
-                                                                  for
-                                                                  row
-                                                                  in data_timeseries]
-
-                output_dataset.variables['VEL'][:, :] = [[float(value) for value in row[5::9]] for row in data_timeseries]
-                output_dataset.variables['WIDTH'][:, :] = [[float(value) for value in row[6::9]] for row in data_timeseries]
-                output_dataset.variables['CNR'][:, :] = [[float(value) for value in row[4::9]] for row in data_timeseries]
+                output_dataset.variables['VEL'][:, :] = [[float(value) for value in row[7::8]] for row in data_timeseries]
+                output_dataset.variables['WIDTH'][:, :] = [[float(value) for value in row[5::8]] for row in data_timeseries]
+                output_dataset.variables['CNR'][:, :] = [[float(value) for value in row[4::8]] for row in data_timeseries]
+                
                 
             else:
-                timestamp_input = [datetime.datetime.strptime(row[0],'%Y/%m/%d %H:%M') for row in data_timeseries]
+                timestamp_input = [datetime.datetime.strptime(row[0],'%d/%m/%Y %H:%M:%S') for row in data_timeseries]
                 timestamp_iso8601 = [value.isoformat()+'Z' for value in timestamp_input]
                 output_dataset.variables['time'][:] = np.array(timestamp_iso8601)
-                output_dataset.variables['T_internal'][:] = [float(row[1]) for row in data_timeseries]
-                output_dataset.variables['wiper'][:] = [float(row[6]) for row in data_timeseries]
-                output_dataset.variables['WS'][:, :] = [[float(value) for value in row[7::12]] for row in data_timeseries]
-                output_dataset.variables['WIDTH'][:, :] = [[float(value) for value in row[16::12]] for row in data_timeseries]
-                output_dataset.variables['CNR'][:, :] = [[float(value) for value in row[14::12]] for row in data_timeseries]
-        
+                output_dataset.variables['wiper'][:] = [float(row[1]) for row in data_timeseries]
+                output_dataset.variables['WS'][:, :] = [[float(value) for value in row[3::19]] for row in data_timeseries]
+                output_dataset.variables['WIDTH'][:, :] = [[float(value) for value in row[18::19]] for row in data_timeseries]
+                output_dataset.variables['CNR'][:, :] = [[float(value) for value in row[14::19]] for row in data_timeseries]
---
+++ lidaco/readers/ZephIR300.py
@@ -0,0 +1,143 @@
+import numpy as np
+from ..core.Reader import Reader
+import datetime
+import pandas as pd
+import re
+
+
+class ZephIR300(Reader):
+
+    def __init__(self):
+        super().__init__(False)
+
+    def accepts_file(self, filename):
+        return filename.endswith('.csv')
+
+    def output_filename(self, filename):
+        return filename[:-4]
+
+    def parse_time(string1):
+            temp = datetime.datetime.strptime(string1,'%d.%m.%Y %H:%M:%S')
+            temp = temp.isoformat() +'Z'
+            return temp
+
+#    def required_params(self):
+#        return ['position_x_input', 'position_y_input', 'position_z_input']
+
+    def read_to(self, output_dataset, input_filepath, configs, appending):
+
+        # read file
+        
+        ten_min_file = (re.findall(r'(?<=\\)\w+(?=@)',input_filepath)[0] == r'Wind10_317')
+
+
+        df=pd.read_csv(input_filepath,sep=';',skiprows=1,decimal=',')
+        
+
+        with open(input_filepath,'r',encoding='latin-1') as f: # get parameters from header
+            header = f.readline()
+            header=header.split(';')
+            parameters = {line.split(':')[0].strip(): line.split(':')[1].strip() for line in header if ':' in line}
+            parameters['Measurement heights'] = [int(element.strip()) for element in parameters['Measurement heights'].split('m') if (element != '')]
+            parameters['Measurement heights'].append(1)
+
+
+
+        # create the dimensions
+        output_dataset.createDimension('range', len(parameters['Measurement heights']))
+        output_dataset.createDimension('time', None)
+
+        # create the coordinate variables
+        range1 = output_dataset.createVariable('range', 'f4', ('range',))
+        range1.units = 'm'
+        range1.long_name = 'range_gate_distance_from_lidar'
+        range1[:] = np.array(parameters['Measurement heights'])
+
+        time = output_dataset.createVariable('time', str, ('time',))
+        time.units = 's'
+        time.long_name = 'timestamp ISO 8601'
+
+        # create the data variables
+        scan_type = output_dataset.createVariable('scan_type', 'i')
+        scan_type.units = 'none'
+        scan_type.long_name = 'scan_type_of_the_measurement'
+        scan_type[:] = 1
+
+        accumulation_time = output_dataset.createVariable('accumulation_time', 'f4')
+        accumulation_time.units = 'seconds'
+        accumulation_time.long_name = 'time_for_spectral_accumulation'
+        accumulation_time[:] = 1.0
+
+        # create the measurement variables
+        
+        # Beschreibung einfügen
+        tilt = output_dataset.createVariable('tilt', 'f4', ('time',))
+        tilt.units = 'degrees north'
+        tilt.long_name = 'either pitch or roll depending on higher value'
+
+        T_external = output_dataset.createVariable('T_external', 'f4', ('time',))
+        T_external.units = 'degrees C'
+        T_external.long_name = 'temperature'
+
+        yaw = output_dataset.createVariable('yaw', 'f4', ('time',))
+        yaw.units = 'degrees'
+        yaw.long_name = 'lidar_yaw_angle'
+
+        rh = output_dataset.createVariable('rh', 'f4', ('time',))
+        rh.units = 'degrees'
+        rh.long_name = 'lidar_yaw_angle'
+        
+        p = output_dataset.createVariable('p', 'f4', ('time',))
+        p.units = 'degrees'
+        p.long_name = 'lidar_yaw_angle'
+        
+        wiper = output_dataset.createVariable('wiper', 'f4', ('time',))
+        wiper.units = 'V'
+        wiper.long_name = 'Wiper count Vbatt'
+
+        WS = output_dataset.createVariable('WS', 'f4', ('time', 'range'))
+        WS.units = 'm.s-1'
+        WS.long_name = 'mean of scalar wind speed'
+        
+        DIR = output_dataset.createVariable('DIR', 'f4', ('time', 'range'))
+        DIR.units = 'degrees north'
+        DIR.long_name = 'wind direction from north'
+
+        # fill values from dataset
+        
+        df['timestamp_iso8601'] = df['Time and Date'].apply(ZephIR300.parse_time)
+        output_dataset.variables['time'][:] = df['timestamp_iso8601'].values
+
+#        data_timeseries = [row.strip().split(';') for row in data[2:]]
+
+        output_dataset.variables['T_external'][:] = df['Air Temp. (C)'].values
+        
+        output_dataset.variables['tilt'][:] = df['Tilt (deg)'].values
+        output_dataset.variables['yaw'][:] = df['ZephIR Bearing (deg)'].values
+        output_dataset.variables['rh'][:] = df['Humidity (%)'].values
+        output_dataset.variables['p'][:] = df['Pressure (mbar)'].values
+        output_dataset.variables['wiper'][:] = df['Battery (V)'].values
+        # e.g. radial velocity starts at 5th column and is then repeated every 9th column
+        
+        met_ws_list = df.iloc[:,16]
+        met_dir_list = df.iloc[:,17]
+        
+
+        if ten_min_file:
+            ws_list = df.iloc[:,21:-1:8]
+            dir_list = df.iloc[:,20:-1:8]
+
+        else:
+            ws_list = df.iloc[:,20:-1:3]
+            dir_list = df.iloc[:,19:-1:3]
+        
+        
+        
+        ws_list_complete = pd.concat([ws_list,met_ws_list],join='inner',axis=1)
+        dir_list_complete = pd.concat([dir_list,met_dir_list],join='inner',axis=1)
+
+        output_dataset.variables['WS'][:, :] = ws_list_complete.values
+        output_dataset.variables['DIR'][:, :] = dir_list_complete.values
+        
+
+
